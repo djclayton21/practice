299c6824dbbfd26100fd576b1ad313cf89018994
djclayton21 <clayton.daniel.j@gmail.com>
2019-11-24 17:24:20 -0500

    finish practice problem

--- code-signal/reverseInParenthesis.js
+++ code-signal/reverseInParenthesis.js
@@ -14,9 +14,23 @@ const test4 = "foo(bar(baz))blim";
 const result4 = "foobazrabblim";
 
 function reverseInParentheses(inputString) {
-    //find parentheses (right to left)
-    //take out section, flip, replace
-    //recursively for funsies?
+    //find parentheses
+    let open = 0;
+    let close = 0;
+    let result = inputString;
+    let isFlipped = false;
+    for (let i = 0; i < inputString.length; i++){
+        if (inputString[i] === "(") {
+            open = i;
+        }
+        if (inputString[i] === ")") {
+            close = i;
+            result = inputString.slice(0, open) + removeAndReverse(inputString.slice(open, close + 1)) + inputString.slice(close + 1);
+            isFlipped = true;
+            break
+        } 
+    }
+    return isFlipped ? reverseInParentheses(result) : result;
 }
 
 //take section, remove (), return flipped
@@ -24,8 +38,6 @@ function removeAndReverse(inputSection) {
     let section = inputSection.slice(1, -1);
     return section.split("").reverse().join("");
 }
-
-console.log(removeAndReverse(test1))
 
 console.log("test 1:", reverseInParentheses(test1) === result1);
 console.log("test 2:", reverseInParentheses(test2) === result2);
