65ccd8c03d94178f4402ab0f9eff39178514b60f
djclayton21 <clayton.daniel.j@gmail.com>
2019-12-11 16:06:55 -0700

    prettier

--- code-signal/alternatingSums.js
+++ code-signal/alternatingSums.js
@@ -1,4 +1,4 @@
-//Several people are standing in a row and need to be divided into two teams. 
+//Several people are standing in a row and need to be divided into two teams.
 //The first person goes into team 1, the second goes into team 2,
 // the third goes into team 1 again, the fourth into team 2, and so on.
 
@@ -9,24 +9,24 @@
 const test1 = [50, 60, 60, 45, 70]; //[180, 105]
 const test2 = [100, 50]; //[100, 50]
 const test3 = [80]; //[80, 0]
-const test4 = [100, 50, 50, 100] //[150, 150]
+const test4 = [100, 50, 50, 100]; //[150, 150]
 
 function alternatingSums(a) {
-    const result = [0 , 0];
-    //loop
-    for (let i = 0; i < a.length; i++){
-        //if even index or 0, add to first section
-        if (i % 2 === 0) {
-            result[0] += a[i];
-        } else {
-            result[1] += a[i];
-        }
-        //else add two second
-    }
-    return result;
+	const result = [0, 0];
+	//loop
+	for (let i = 0; i < a.length; i++) {
+		//if even index or 0, add to first section
+		if (i % 2 === 0) {
+			result[0] += a[i];
+		} else {
+			result[1] += a[i];
+		}
+		//else add two second
+	}
+	return result;
 }
 
-console.log(alternatingSums(test1))
-console.log(alternatingSums(test2))
-console.log(alternatingSums(test3))
-console.log(alternatingSums(test4))
+console.log(alternatingSums(test1));
+console.log(alternatingSums(test2));
+console.log(alternatingSums(test3));
+console.log(alternatingSums(test4));
--- code-signal/reverseInParenthesis.js
+++ code-signal/reverseInParenthesis.js
@@ -1,45 +1,51 @@
 // Write a function that reverses characters in (possibly nested) parentheses in the input string.
 
 // Input strings will always be well-formed with matching ()s.
-const test1 = "(bar)"; 
-const result1 = "rab";
+const test1 = '(bar)';
+const result1 = 'rab';
 
-const test2 = "foo(bar)baz";
-const result2 = "foorabbaz";
+const test2 = 'foo(bar)baz';
+const result2 = 'foorabbaz';
 
-const test3 = "foo(bar)baz(blim)"; 
-const result3 = "foorabbazmilb";
+const test3 = 'foo(bar)baz(blim)';
+const result3 = 'foorabbazmilb';
 
-const test4 = "foo(bar(baz))blim";
-const result4 = "foobazrabblim";
+const test4 = 'foo(bar(baz))blim';
+const result4 = 'foobazrabblim';
 
 function reverseInParentheses(inputString) {
-    //find parentheses
-    let open = 0;
-    let close = 0;
-    let result = inputString;
-    let isFlipped = false;
-    for (let i = 0; i < inputString.length; i++){
-        if (inputString[i] === "(") {
-            open = i;
-        }
-        if (inputString[i] === ")") {
-            close = i;
-            result = inputString.slice(0, open) + removeAndReverse(inputString.slice(open, close + 1)) + inputString.slice(close + 1);
-            isFlipped = true;
-            break
-        } 
-    }
-    return isFlipped ? reverseInParentheses(result) : result;
+	//find parentheses
+	let open = 0;
+	let close = 0;
+	let result = inputString;
+	let isFlipped = false;
+	for (let i = 0; i < inputString.length; i++) {
+		if (inputString[i] === '(') {
+			open = i;
+		}
+		if (inputString[i] === ')') {
+			close = i;
+			result =
+				inputString.slice(0, open) +
+				removeAndReverse(inputString.slice(open, close + 1)) +
+				inputString.slice(close + 1);
+			isFlipped = true;
+			break;
+		}
+	}
+	return isFlipped ? reverseInParentheses(result) : result;
 }
 
 //take section, remove (), return flipped
 function removeAndReverse(inputSection) {
-    let section = inputSection.slice(1, -1);
-    return section.split("").reverse().join("");
+	let section = inputSection.slice(1, -1);
+	return section
+		.split('')
+		.reverse()
+		.join('');
 }
 
-console.log("test 1:", reverseInParentheses(test1) === result1);
-console.log("test 2:", reverseInParentheses(test2) === result2);
-console.log("test 3:", reverseInParentheses(test3) === result3);
-console.log("test 4:", reverseInParentheses(test4) === result4);
+console.log('test 1:', reverseInParentheses(test1) === result1);
+console.log('test 2:', reverseInParentheses(test2) === result2);
+console.log('test 3:', reverseInParentheses(test3) === result3);
+console.log('test 4:', reverseInParentheses(test4) === result4);
